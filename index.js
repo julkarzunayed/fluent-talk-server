289eea527c74e9aba714547f9bfb59dfd00fd753
julkarzunayed <zunayedjulkar@gmail.com>
2025-06-14 14:57:01 +0600

    added mongoDB and user post API

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpoctao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const usersCollections = client.db('fluent_talk').collection('users');
+        const tutorialsCollections = client.db('fluent_talk').collection('tutorials');
+
+
+        //users APIs
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            const result = await usersCollections.insertOne(userInfo);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send(`FluentTalk server is cooking`)
